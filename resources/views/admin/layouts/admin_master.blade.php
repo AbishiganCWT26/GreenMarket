8c69071ff64d396f8b00e476078602af0e6f122d
R. Abishigan <trincoabishigan@gmail.com>
2026-03-17 13:27:36 +0530

    GreenMarket 1.0.0.86

--- resources/views/admin/layouts/admin_master.blade.php
+++ resources/views/admin/layouts/admin_master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GreenMarket | @yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/admin-master.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/Admin/admin-master.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     @yield('styles')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
