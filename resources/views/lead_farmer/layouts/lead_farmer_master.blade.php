dfd926e197e7515bec8650df5145e354351a1127
R. Abishigan <trincoabishigan@gmail.com>
2026-04-21 13:27:14 +0530

    GreenMarket 1.0.0.127 21/04 13.26

--- resources/views/lead_farmer/layouts/lead_farmer_master.blade.php
+++ resources/views/lead_farmer/layouts/lead_farmer_master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lead Farmer @yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/Lead_Farmer/lead_farmer-master.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/lead_farmer/lead_farmer-master.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .swal2-image {
