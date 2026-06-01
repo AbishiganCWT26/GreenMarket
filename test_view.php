aef96ba48039a74fab3b758969235fc58c5a9945
R. Abishigan <trincoabishigan@gmail.com>
2026-06-01 17:03:43 +0530

    GreenMarket 1.0.0.132 01/06 17.03

--- test_view.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-$view = view('contactus')->withErrors(new \Illuminate\Support\MessageBag())->render();
-file_put_contents('rendered_contact.html', $view);
-echo "Rendered successfully.\n";
